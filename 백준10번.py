4ea77e17f292d21f008b633d1d1d1fa6e7c32386
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-07-04 17:15:31 +0900

    Add files via upload

---
+++ 백준10번.py
@@ -0,0 +1,6 @@
+a = int(input())
+
+
+for i in range(a):
+    n,m= map(int,input().split)
+    print(n+m)
---
+++ 백준11번.py
@@ -0,0 +1,9 @@
+n= int(input())
+m = input()
+
+
+print(n*int[2])
+print(n*int[1])
+print(n*int[0])
+
+print(n*int[m])
