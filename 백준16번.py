f454745183b2be65f4d463adcb440ae14c04a9c8
shinjaewon99 <95893341+shinjaewon99@users.noreply.github.com>
2022-07-05 14:11:41 +0900

    Add files via upload

---
+++ 백준16번.py
@@ -0,0 +1,14 @@
+a = int(input())
+
+num = list(map(int, input().split()))
+
+max = num[0]
+min = num[0]
+
+for i in num[1: a]:
+    if i > max:
+        max = i
+    elif i < min:
+        min = i
+
+print(min , max)
